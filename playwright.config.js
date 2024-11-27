40e10f0302b850d9f9e9211d268da1b851ba836c
tebrex1995 <tvrdisica@gmail.com>
2024-11-27 01:12:19 +0100

    added allure-reporter

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
-// @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import allurePlaywright from 'allure-playwright';
 
 export default defineConfig({
   testDir: './src/tests',
@@ -8,16 +8,25 @@ export default defineConfig({
   fullyParallel: true,
 
   forbidOnly: !!process.env.CI,
-
   retries: process.env.CI ? 2 : 0,
-
   workers: process.env.CI ? 1 : undefined,
 
-  reporter: 'html',
+  reporter: [
+    ['line'],
+    ['html'],
+    [
+      'allure-playwright',
+      {
+        outputFolder: 'allure-results',
+      },
+    ],
+  ],
 
   use: {
+    expect: {
+      timeout: 30000,
+    },
     baseURL: 'https://www.globalsqa.com',
-
     trace: 'on-first-retry',
   },
 
